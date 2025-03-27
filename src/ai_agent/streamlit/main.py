e34edb4748fb495b357b540802aaac13018ecad9
kurousa <6209225+kurousa@users.noreply.github.com>
2025-03-28 07:12:20 +0900

    mod: unused import

--- src/ai_agent/streamlit/main.py
+++ src/ai_agent/streamlit/main.py
@@ -4,10 +4,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 try:
-    import os
     from dotenv import load_dotenv
     load_dotenv()
     # [debug]check envs
+    # import os
     # environment_variables = os.environ
     # for key, value in environment_variables.items():
     #   print(f"{key}={value}")
